0dc4d2ddc2c3364f667eb82b5c584c354948b103
cj2001 <cj2001@gmail.com>
2021-06-21 16:59:43 -0600

    feat: basic functionality for embeddings

--- src/twitch_demo.py
+++ src/twitch_demo.py
@@ -13,6 +13,21 @@ st.sidebar.title('Basic graph interface')
 
 emb = 'FastRP'
 
+##############################
+#
+#   Sidebar content
+#
+##############################
+
+if st.sidebar.button('List existing graphs'):
+    list_graph_query = """CALL gds.graph.list()"""
+    result = neo4j_utils.query(list_graph_query)
+    if result:
+        for el in result:
+            st.sidebar.write(el[1])
+    else:
+        st.sidebar.write('There are currently no graphs in memory.')
+
 create_graph = st.sidebar.text_input('Name of graph to be created: ')
 if st.sidebar.button('Create in-memory graph'):
     create_graph_query = """CALL gds.graph.create(
@@ -25,35 +40,36 @@ if st.sidebar.button('Create in-memory graph'):
     result = neo4j_utils.query(create_graph_query)
     st.sidebar.write('Graph ', result[0][2], 'has ', result[0][3], 'nodes and ', result[0][4],' relationships.')
 
-emb = st.sidebar.selectbox('Choose an embedding: ', ['FastRP', 'node2vec'])
-st.write('You selected: ', emb)
-
-dim = st.sidebar.slider('Embedding dimension: ', value=10, min_value=2, max_value=50)
-st.write('We will use ', dim, ' dimensions for the ', emb, ' embedding.')
-
-
-
-num_nodes = neo4j_utils.query('MATCH(n) RETURN COUNT(n)')
-
-st.write('Nodes in graph: ', num_nodes[0][0])
-
-# create_graph = st.text_input('Name of graph to be created: ')
-# if st.button('Create in-memory graph'):
-#     create_graph_query = """CALL gds.graph.create(
-#                                     '{}',
-#                                     '*',
-#                                     '*'
-#                             )
-#                          """.format(create_graph)
-
-#     st.write(create_graph_query)
-#     result = neo4j_utils.query(create_graph_query)
-#     st.write(result)
-
-drop_graph = st.text_input('Name of graph to be dropped: ')
-if st.button('Drop in-memory graph'):
+drop_graph = st.sidebar.text_input('Name of graph to be dropped: ')
+if st.sidebar.button('Drop in-memory graph'):
     drop_graph_query = """CALL gds.graph.drop('{}')""".format(drop_graph)
     result = neo4j_utils.query(drop_graph_query)
-    st.write(result)
+    st.sidebar.write('Graph ', result[0][0],' has been dropped.')
+
+emb = st.sidebar.selectbox('Choose an embedding: ', ['FastRP', 'node2vec'])
+
+dim = st.sidebar.slider('Embedding dimension: ', value=10, min_value=2, max_value=50)
+
+emb_graph = st.sidebar.text_input('Enter graph name for embedding creation:')
+
+if st.sidebar.button('Create embeddings'):
+    if emb == 'FastRP':
+        emb_query = """CALL gds.fastRP.write('%s', {
+                          embeddingDimension: 10, 
+                          writeProperty: 'frp_all_nodes'}
+                    )""" % (emb_graph)
+        result = neo4j_utils.query(emb_query)
+    else:
+        st.write('No embs created')
+
+##############################
+#
+#   Main panel content
+#
+##############################
+
+
+
+
 
 
