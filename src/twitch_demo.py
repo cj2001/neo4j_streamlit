44a2ca1ce482e45c8d2881619a312eccfd685bc6
cj2001 <cj2001@gmail.com>
2021-06-21 16:09:07 -0600

    feat: connecting to Sandbox

--- src/twitch_demo.py
+++ src/twitch_demo.py
@@ -1,13 +1,13 @@
-import numpy
-import pandas
+import numpy as np
+import pandas as pd
 
-import graphistry
-import networkx
 import streamlit as st
 
-graphistry.register(api=3, protocol="https", server="hub.graphistry.com", 
-                    token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VybmFtZSI6ImNqIiwiaWF0IjoxNjIzNjkyODM3LCJleHAiOjE2MjM2OTY0MzcsImp0aSI6IjkzNDgwODQzLTBlZjUtNDZlMC1iOTUyLTY3YTUwMmJmNjgzMyIsInVzZXJfaWQiOjE4ODksIm9yaWdfaWF0IjoxNjIzNjkyODM3fQ.6tI92r3bMOilIFCvCaELcaHPtch4T24O6cWDfXFQSxs"
-                    )
+from neo4j_utils import Neo4jConnection
+
+neo4j_utils = Neo4jConnection(uri='bolt://3.231.58.8:7687', 
+                              user='neo4j',
+                              pwd='band-thermometer-sash')
 
 st.sidebar.title('Basic graph interface')
 
@@ -19,6 +19,31 @@ st.write('You selected: ', emb)
 dim = st.sidebar.slider('Embedding dimension: ', value=10, min_value=2, max_value=50)
 st.write('We will use ', dim, ' dimensions for the ', emb, ' embedding.')
 
-G = nx.complete_graph(100)
+num_nodes = neo4j_utils.query('MATCH(n) RETURN COUNT(n)')
+
+st.write('Nodes in graph: ', num_nodes[0][0])
+
+if st.button('Create df'):
+    query = """MATCH (p:Person)-[:BELONGS_TO]->(h:House)
+               RETURN p.name AS name, h.name AS house
+               ORDER BY h.name, p.name"""
+    df = pd.DataFrame([dict(_) for _ in neo4j_utils.query(query)])
+    st.dataframe(df.style.hide_index())
+
+if st.button('Create in-memory graph'):
+    create_graph_query = """CALL gds.graph.create(
+                                    'all',
+                                    '*',
+                                    '*'
+                            )
+                         """
+
+    result = neo4j_utils.query(create_graph_query)
+    st.write(result)
+
+if st.button('Drop in-memory graph'):
+    drop_graph_query = """CALL gds.graph.drop('all')"""
+    result = neo4j_utils.query(drop_graph_query)
+    st.write(result)
 
 
