3cf5a5f7bcec695ce8c2899f7a10bae974b21b9d
cj2001 <cj2001@gmail.com>
2021-06-21 17:18:24 -0600

    fix: organized things a bit more

--- src/twitch_demo.py
+++ src/twitch_demo.py
@@ -28,6 +28,8 @@ if st.sidebar.button('List existing graphs'):
     else:
         st.sidebar.write('There are currently no graphs in memory.')
 
+st.sidebar.markdown("""---""")
+
 create_graph = st.sidebar.text_input('Name of graph to be created: ')
 if st.sidebar.button('Create in-memory graph'):
     create_graph_query = """CALL gds.graph.create(
@@ -40,15 +42,25 @@ if st.sidebar.button('Create in-memory graph'):
     result = neo4j_utils.query(create_graph_query)
     st.sidebar.write('Graph ', result[0][2], 'has ', result[0][3], 'nodes and ', result[0][4],' relationships.')
 
+st.sidebar.markdown("""---""")
+
 drop_graph = st.sidebar.text_input('Name of graph to be dropped: ')
 if st.sidebar.button('Drop in-memory graph'):
     drop_graph_query = """CALL gds.graph.drop('{}')""".format(drop_graph)
     result = neo4j_utils.query(drop_graph_query)
     st.sidebar.write('Graph ', result[0][0],' has been dropped.')
 
+st.sidebar.markdown("""---""")
+
 emb = st.sidebar.selectbox('Choose an embedding: ', ['FastRP', 'node2vec'])
 
 dim = st.sidebar.slider('Embedding dimension: ', value=10, min_value=2, max_value=50)
+
+##############################
+#
+#   Main panel content
+#
+##############################
 
 emb_graph = st.text_input('Enter graph name for embedding creation:')
 
@@ -73,15 +85,6 @@ if st.button('Create embeddings'):
 if st.button('Drop embeddings'):
     neo4j_utils.query('MATCH (n) REMOVE n.frp_emb')
     neo4j_utils.query('MATCH (n) REMOVE n.n2v_emb')
-
-##############################
-#
-#   Main panel content
-#
-##############################
-
-
-
 
 
 
